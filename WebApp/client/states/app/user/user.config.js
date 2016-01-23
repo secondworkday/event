0ebf846decfaae87776c753d9067bd52686ea076
Jeremy Mercer <Jeremy@MercerSoftware.com>
2016-01-23 01:14:14 -0800

    Adds event page tabs stuff

--- WebApp/client/states/app/user/user.config.js
+++ WebApp/client/states/app/user/user.config.js
@@ -16,6 +16,7 @@ app.config(['$stateProvider', 'AUTHORIZATION_ROLES', function ($stateProvider, A
       url: '/event-old',
       templateUrl: '/client/states/app/user/event-old.html',
     })
+/*
     .state('app.user.event', {
       url: '/event/:eventID/',
       templateUrl: '/client/states/app/user/event.html',
@@ -38,6 +39,85 @@ app.config(['$stateProvider', 'AUTHORIZATION_ROLES', function ($stateProvider, A
         }
       }
     })
+*/
+    .state('app.user.event', {
+      abstract: true,
+      url: '/event/:eventID',
+      templateUrl: '/client/states/app/user/event.html',
+      controller: "EventController",
+      resolve: {
+        event: function ($stateParams, siteService) {
+          var eventID = $stateParams.eventID;
+          return siteService.ensureEvent(eventID);
+        },
+        eventSessionsIndex: function (siteService, $stateParams) {
+          var eventID = $stateParams.eventID;
+          return siteService.ensureEventSessions(eventID);
+        },
+        eventParticipantsIndex: function (siteService, $stateParams) {
+          var eventID = $stateParams.eventID;
+          return siteService.ensureEventParticipantsIndex(eventID);
+        },
+        eventParticipantGroupsIndex: function (siteService, eventParticipantsIndex) {
+          return siteService.ensureEventParticipantGroups(siteService, eventParticipantsIndex);
+        }
+      },
+      data: {
+        pageTitle: 'Back',
+        backState: 'app.job-seeker.career-step.home'
+      }
+    })
+    .state('app.user.event.sessions', {
+      url: '/sessions',
+      data: {
+        'selectedTab': 0
+      },
+      views: {
+        'sessions': {
+          templateUrl: '/client/states/app/user/participants.html',
+          controller: "EventParticipantsController"
+        }
+      }
+    })
+    .state('app.user.event.schools', {
+      url: '/schools',
+      data: {
+        'selectedTab': 1
+      },
+      views: {
+        'schools': {
+          templateUrl: '/client/states/app/user/participants.html'
+        }
+      }
+    })
+    .state('app.user.event.participants', {
+      url: '/participants',
+      data: {
+        'selectedTab': 2
+      },
+      views: {
+        'participants': {
+          templateUrl: '/client/states/app/user/participants.html'
+        }
+      }
+    })
+    .state('app.user.event.documents', {
+      url: '/documents',
+      data: {
+        'selectedTab': 3
+      },
+      views: {
+        'documents': {
+          templateUrl: '/client/states/app/user/participants.html'
+        }
+      }
+    })
+
+
+
+
+
+
 
     .state('app.user.event-participants', {
       url: '/event/:eventID/participants',
