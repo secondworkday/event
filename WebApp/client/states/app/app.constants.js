99b7b709451eef9ffb16a1244f3b75159f5c8fa5
kiley0 <kileydorton@gmail.com>
2016-02-05 22:19:44 -0500

    adds a new js file to keep all of our CONSTANTS in one place, including the ALE-specific UI terminology

---
+++ WebApp/client/states/app/app.constants.js
@@ -0,0 +1,34 @@
+app.constant('CONSTANTS', {
+  months: [
+    {name: "January", number: 00},
+    {name: "February", number: 01},
+    {name: "March", number: 02},
+    {name: "April", number: 03},
+    {name: "May", number: 04},
+    {name: "June", number: 05},
+    {name: "July", number: 06},
+    {name: "August", number: 07},
+    {name: "September", number: 08},
+    {name: "October", number: 09},
+    {name: "November", number: 10},
+    {name: "December", number: 11}
+  ],
+  years: [
+    2015, 2014, 2013, 2012, 2011, 2010, 2009, 2008, 2007, 2006, 2005, 2004, 2003,
+    2002, 2001, 2000, 1999, 1998, 1997, 1996, 1995, 1994, 1993, 1992, 1991, 1990,
+    1989, 1988, 1987, 1986, 1985, 1984, 1983, 1982, 1981, 1980, 1979, 1978, 1977,
+    1976, 1975, 1974, 1973, 1972, 1971, 1970, 1969, 1968, 1967, 1966, 1965, 1964,
+    1963, 1962, 1961, 1960, 1959, 1958, 1957, 1956, 1955, 1954, 1953, 1952, 1951,
+    1950
+  ],
+  timeZones: [
+    { name: "UTC",      index: 1},
+    { name: "Eastern",  index: 2 },
+    { name: "Central",  index: 3 },
+    { name: "Arizona",  index: 4 },
+    { name: "Mountain", index: 5 },
+    { name: "Pacific",  index: 6 },
+    { name: "Alaska",   index: 7 },
+    { name: "Hawaii",   index: 8 }
+  ]
+});
