4a229753607b7f3b62a8cd34184b3b30ebc3dac6
Jeremy Mercer <Jeremy@MercerSoftware.com>
2016-02-22 13:01:34 -0800

    Add App-level ActivityTypes

---
+++ AppLibrary/Domain/ActivityLog.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Mail;
+using System.Threading;
+using System.Net;
+using System.IO;
+using System.Xml.Linq;
+using System.Web;
+
+using MS.Utility;
+
+namespace App.Library
+{
+    public class ActivityType : MS.Utility.ActivityType
+    {
+        protected ActivityType(int applicationID) :
+            base(applicationID)
+        { }
+
+        protected static ActivityType create(int id) { return new ActivityType(id); }
+
+        public static readonly ActivityType BulkCheckIn = create(0);
+        public static readonly ActivityType BulkUndoCheckIn = create(1);
+    }
+}
